494e4766f9b6d757f7bb8d121d6b218d804645ab
arabanus <anton.rabanus@gmail.com>
2024-11-20 14:44:26 +0100

    initial commit

---
+++ particle_simulation/main_classes.py
@@ -0,0 +1,39 @@
+"""Main classes to run the simulation
+"""
+
+class ParticleField:
+    def __init__(self) -> None:
+        pass
+    
+    def prepare_field(self):
+        pass
+
+    def simulate(self):
+        pass
+        
+class Particle:
+    def __init__(self) -> None:
+        pass
+
+    def shape(self):
+        pass
+
+    def start_position_speed(self):
+        pass
+
+    def movement(self):
+        pass
+
+    def color(self):
+        pass
+
+    def characteristics(self, influence, friction, random_movement):
+        pass
+
+
+class InteractionMatrix:
+    def __init__(self) -> None:
+        pass
+
+    def relationships(self):
+        pass
---
+++ particle_simulation/particle_classes.py
@@ -0,0 +1,26 @@
+"""Particles that use the main particle class
+"""
+from particle_simulation.particle_classes import Particle
+
+
+class Particle_A(Particle):
+    def __init__(self) -> None:
+        pass
+
+    self.char
+
+
+class Particle_B(Particle):
+    def __init__(self) -> None:
+        pass        
+
+
+class Particle_C(Particle):
+    def __init__(self) -> None:
+        pass        
+
+
+class Particle_D(Particle):
+    def __init__(self) -> None:
+        pass
+        
